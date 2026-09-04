b8cf8d925e3402443eba690c3171a2b91c943276
Kyle King <git@kylek.ing>
2026-09-04 08:46:55 -0700

    doc: fix feature gated docs

--- crates/lib-conf/src/lib.rs
+++ crates/lib-conf/src/lib.rs
@@ -2,4 +2,6 @@
 #![deny(rustdoc::broken_intra_doc_links)]
 #![doc = include_str!("../README.md")]
 
+#[cfg(feature = "derive")]
+#[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
 pub use lib_conf_derive::LibConfig;
